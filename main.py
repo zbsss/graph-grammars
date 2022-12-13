1214043391e3814596a38a9d531b26f9d8baed08
enkelian <aniano13@gmail.com>
2022-12-13 15:04:39 +0100

    initial tests

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from utils.graph_drawer import draw_graph
 from productions.p1 import P1
 from productions.p2 import P2
-
+from productions.p5 import P5
 
 def main():
     P1(0)
-    P2(5)
-
-    draw_graph()
+    P5(5)
+    # draw_graph()
 
 if __name__ == "__main__":
     main()
---
+++ productions/p5.py
@@ -0,0 +1,12 @@
+from utils.common import *
+from utils.vertex import VertexLabel
+from utils.sort_utils import sort_graph_fragments
+
+def P5(id: int):
+    global vertices_graph_fragment
+    global graph_fragment_list
+
+    frag = vertices_graph_fragment[id]
+    print(frag)
+
+    f1 =
