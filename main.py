f4047df9f92677150fa7adced095eb2c5fdf6c69
Jakub Mysliwiec <jakub.mysliwiec@swmansion.com>
2022-12-19 16:05:33 +0100

    cleanup main

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 from utils.graph_drawer import draw_graph
 from productions.p1 import P1
 from productions.p2 import P2
-from productions.p8 import P8
-from productions.p7 import P7
-from utils.common import *
 
 def main():
     P1(0)
     P2(5)
-    P2(10)
-    P2(15)
-    graph_fragment_upper_left = vertices_graph_fragment.get(35)
-    graph_fragment_upper_right = vertices_graph_fragment.get(50)
-    upper_right_vertex = get_upper_left_vertice_in_graph_fragment(graph_fragment_upper_right)
-    upper_left_vertex = get_upper_right_vertice_in_graph_fragment(graph_fragment_upper_left)
-    merge_verticies(upper_right_vertex, upper_left_vertex, [graph_fragment_upper_right])
-
 
     draw_graph()
 
