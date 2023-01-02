c11f8ed11861e8aac193fb1226fb97e9073eb9dc
Karol Koptyra <koptyrakarol@gmail.com>
2023-01-02 23:17:33 +0100

    fix tests for p3

--- tests/p3_tests.py
+++ tests/p3_tests.py
@@ -1,14 +1,20 @@
-from productions.p1 import P1
-from productions.p3 import P3
-from utils.graph_drawer import draw_graph
-from utils.graph_fragment import GraphFragment
+import productions.p3, productions.p1, utils.common
 from utils.vertex import Vertex, VertexLabel
-from utils.common import vertices_graph_fragment
-import unittest
+import unittest, importlib
+
+def reload_libs():
+    importlib.reload(utils.common)
+    importlib.reload(productions.p1)
+    importlib.reload(productions.p3)
 
 class P3TestMethods(unittest.TestCase):
     # correct circumstances - 1 broken edge with vertex in between
     def test_one_broken_edge(self):
+        reload_libs()
+        P1 = productions.p1.P1
+        P3 = productions.p3.P3
+        vertices_graph_fragment = utils.common.vertices_graph_fragment
+
         P1(0)
         graph_fragment = vertices_graph_fragment.get(5)
         graph_fragment.vertices.extend(list([Vertex(0, -1.5, 106, VertexLabel(1))]))
@@ -19,6 +25,11 @@ class P3TestMethods(unittest.TestCase):
 
     # incorrect circumstances - 2 broken edges with vertex in between of them
     def test_too_many_broken_edges(self):
+        reload_libs()
+        P1 = productions.p1.P1
+        P3 = productions.p3.P3
+        vertices_graph_fragment = utils.common.vertices_graph_fragment
+
         P1(0)
         graph_fragment = vertices_graph_fragment.get(5)
         graph_fragment.vertices.extend(list([Vertex(0, -1.5, 106, VertexLabel(1)),  Vertex(0.5, -1, 107, VertexLabel(1))]))
@@ -30,6 +41,11 @@ class P3TestMethods(unittest.TestCase):
 
     # incorrect circumstances - not a single broken edge
     def test_no_broken_edges(self):
+        reload_libs()
+        P1 = productions.p1.P1
+        P3 = productions.p3.P3
+        vertices_graph_fragment = utils.common.vertices_graph_fragment
+        
         P1(0)
         graph_fragment = vertices_graph_fragment.get(5)
         P3(5)
@@ -37,6 +53,11 @@ class P3TestMethods(unittest.TestCase):
 
     # incorrect circumstances - 1 broken edge with vertex in between but it's also connected to middle vertex
     def test_broken_edge_vertice_connected_to_middle_vertex(self):
+        reload_libs()
+        P1 = productions.p1.P1
+        P3 = productions.p3.P3
+        vertices_graph_fragment = utils.common.vertices_graph_fragment
+        
         P1(0)
         graph_fragment = vertices_graph_fragment.get(5)
         graph_fragment.vertices.extend(list([Vertex(0, -1.5, 106, VertexLabel(1))]))
@@ -47,6 +68,11 @@ class P3TestMethods(unittest.TestCase):
 
     # incorrect circumstances - 1 broken edge with vertex in between but it's not connected to any other vertex
     def test_broken_edge_vertice_not_connected(self):
+        reload_libs()
+        P1 = productions.p1.P1
+        P3 = productions.p3.P3
+        vertices_graph_fragment = utils.common.vertices_graph_fragment
+        
         P1(0)
         graph_fragment = vertices_graph_fragment.get(5)
         graph_fragment.vertices.extend(list([Vertex(0, -1.5, 106, VertexLabel(1))]))
@@ -55,6 +81,11 @@ class P3TestMethods(unittest.TestCase):
 
     # incorrect circumstances - 1 broken edge with vertex in between but it's in the wrong position (not in the middle of them)
     def test_wrong_position_of_broken_edge(self):
+        reload_libs()
+        P1 = productions.p1.P1
+        P3 = productions.p3.P3
+        vertices_graph_fragment = utils.common.vertices_graph_fragment
+        
         P1(0)
         graph_fragment = vertices_graph_fragment.get(5)
         graph_fragment.vertices.extend(list([Vertex(0, -1.4, 106, VertexLabel(1))]))
@@ -65,6 +96,11 @@ class P3TestMethods(unittest.TestCase):
 
     # incorrect circumstances - incorrect label of at least 1 vertices (excluding middle vertex)
     def test_wrong_label_vertice(self):
+        reload_libs()
+        P1 = productions.p1.P1
+        P3 = productions.p3.P3
+        vertices_graph_fragment = utils.common.vertices_graph_fragment
+
         P1(0)
         graph_fragment = vertices_graph_fragment.get(5)
         graph_fragment.vertices.extend(list([Vertex(0, -1.5, 106, VertexLabel(1))]))
@@ -76,6 +112,11 @@ class P3TestMethods(unittest.TestCase):
 
     # incorrect circumstances - incorrect label of middle vertex (it's not I)
     def test_wrong_label_middle_vertice(self):
+        reload_libs()
+        P1 = productions.p1.P1
+        P3 = productions.p3.P3
+        vertices_graph_fragment = utils.common.vertices_graph_fragment
+
         P1(0)
         graph_fragment = vertices_graph_fragment.get(5)
         graph_fragment.vertices.extend(list([Vertex(0, -1.5, 106, VertexLabel(1))]))
