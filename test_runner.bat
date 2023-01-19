b892e947b16cb51cc006fc407e1aa01a9578b539
Łukasz Pitrus <pitrus@student.agh.edu.pl>
2023-01-19 15:40:35 +0100

    added new tests

--- test_runner.bat
+++ test_runner.bat
@@ -1,6 +1,7 @@
 
 python -m unittest tests.productions.p5.TestP5.test_isomorphism
 python -m unittest tests.productions.p5.TestP5.test_success
+python -m unittest tests.productions.p5.TestP5.test_fail_on_many_few_broken_edges
 python -m unittest tests.productions.p5.TestP5.test_fail_on_invalid_middle_label
 python -m unittest tests.productions.p5.TestP5.test_fail_on_invalid_edge_label
 python -m unittest tests.productions.p5.TestP5.test_fail_on_invalid_middle_coords
@@ -9,6 +10,7 @@ python -m unittest tests.productions.p5.TestP5.test_fail_on_invalid_edges
 
 python -m unittest tests.productions.p6.TestP6.test_isomorphism
 python -m unittest tests.productions.p6.TestP6.test_success
+python -m unittest tests.productions.p6.TestP6.test_fail_on_too_few_broken_edges
 python -m unittest tests.productions.p6.TestP6.test_fail_on_invalid_middle_label
 python -m unittest tests.productions.p6.TestP6.test_fail_on_invalid_edge_label
 python -m unittest tests.productions.p6.TestP6.test_fail_on_invalid_middle_coords
