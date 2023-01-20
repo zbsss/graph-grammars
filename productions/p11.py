13fe111cd7ba4a8218b0e001fb3159d15e46bf54
Jakub Mysliwiec <jakub.mysliwiec@swmansion.com>
2023-01-20 16:55:25 +0100

    Finished zad c

--- productions/p11.py
+++ productions/p11.py
@@ -4,7 +4,11 @@ def P11(i1, i2, i3, E1, E2, E3, E4, E5):
     global vertices_graph_fragment
     vertices_graph_fragment[i3].edges.append((min(E2, E4), max(E2, E4)))
     vertices_graph_fragment[i3].edges.append((min(E2, E5), max(E2, E5)))
-    vertices_graph_fragment[i3].edges.remove((max(E4, E5), min(E4, E5)))  
+    try:
+        vertices_graph_fragment[i3].edges.remove((max(E4, E5), min(E4, E5)))
+    except:
+        vertices_graph_fragment[i3].edges.remove((min(E4, E5), max(E4, E5)))
+
 
     merge_vertices([E1, E4])
     merge_vertices([E3, E5])
