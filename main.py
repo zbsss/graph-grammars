1037ae185a630357fdcf41cc259a960c8ef8bb07
enkelian <aniano13@gmail.com>
2023-01-20 14:13:12 +0100

    p10

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from productions.p1 import P1
+from productions.p10 import P10
 from productions.p2 import P2
 from productions.p9 import P9
 from productions.p3 import P3
@@ -8,10 +9,7 @@ from utils.common import vertices_graph_fragment
 
 def main():
 
-    P1(0)
-    P2(5)
-    P2(20)
-    P2(25)
+    P10(0)
     draw_graph()
 
 if __name__ == "__main__":
